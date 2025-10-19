0d03c392a44d965f403a22e423b6c2e474da0d50
atsushimemet <noap3b69n@gmail.com>
2025-10-19 18:50:54 +0900

    Respect explicit home navigation in RouteRestorer

--- components/RouteRestorer.tsx
+++ components/RouteRestorer.tsx
@@ -4,6 +4,7 @@ import { usePathname, useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 
 const STORAGE_KEY = 'ichidan_return_path'
+const DISABLE_ONCE_KEY = 'ichidan_return_path_disable_once'
 
 export function RouteRestorer() {
   const pathname = usePathname()
@@ -19,15 +20,26 @@ export function RouteRestorer() {
       const currentFullPath = `${window.location.pathname}${window.location.search}${window.location.hash}`
 
       const isAuthCallback = pathname.startsWith('/auth/callback')
+      const disableOnce = sessionStorage.getItem(DISABLE_ONCE_KEY) === '1'
 
-      if (!isAuthCallback && (pathname === '/' || pathname === '') && stored && stored !== '/' && stored !== currentFullPath) {
-        sessionStorage.removeItem(STORAGE_KEY)
-        router.replace(stored)
-        return
-      }
+      if (!isAuthCallback) {
+        if (disableOnce) {
+          sessionStorage.removeItem(DISABLE_ONCE_KEY)
+          sessionStorage.removeItem(STORAGE_KEY)
+          if (pathname === '/' || pathname === '') {
+            return
+          }
+        }
 
-      if (!isAuthCallback && pathname !== '/') {
-        sessionStorage.setItem(STORAGE_KEY, currentFullPath)
+        if ((pathname === '/' || pathname === '') && stored && stored !== '/' && stored !== currentFullPath) {
+          sessionStorage.removeItem(STORAGE_KEY)
+          router.replace(stored)
+          return
+        }
+
+        if (pathname !== '/') {
+          sessionStorage.setItem(STORAGE_KEY, currentFullPath)
+        }
       }
     } catch {
       // セッションストレージが利用できない場合は何もしない
